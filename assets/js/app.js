2ad9d45f7cf1b6aec4e96d4643078f21d3e752bf
Alan Ktquez <ktquez@gmail.com>
2019-11-10 01:58:55 -0300

    Add custom in field file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,13 @@
-// (function (w) {
+(function (w, d) {
 
-// })(window)
+  // File custom
+  const fieldFile = d.getElementById('attachment')
+  const attachmentField = d.querySelector('.attachment-field')
+  const textFileButton = attachmentField.querySelector('.nyx-button--text')
+  fieldFile.addEventListener('change', function (e) {
+    const files = e.target.files
+    if (!files.length) return
+    const btnText = files[0].name.substr(0, 18)
+    textFileButton.innerHTML = files[0].name.length > 18 ? btnText + '...' : btnText
+  }, true)
+})(window, document)
