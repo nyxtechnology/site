4846fce87851eeb9946b1ba8f94956e1a71c9eb9
Alan Ktquez <ktquez@gmail.com>
2020-01-12 11:33:58 -0300

    Add new packaes in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,37 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
+  plugins: [
+    'vue-a11y',
+    'eslint-plugin-import-helpers'
   ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/standard',
+    'plugin:vue-a11y/base'
+  ],
   parserOptions: {
     parser: 'babel-eslint'
-  }
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: [
+          '/^vue/',
+          '/^@vue/',
+          'module',
+          '/^@\//',
+          ['parent', 'sibling', 'index']
+        ],
+        alphabetize: { order: 'asc', ignoreCase: true }
+      }
+    ]
+  }  
 }
