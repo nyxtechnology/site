356f7a24551766fe7057fbee224dbea575ac0a7c
Alan Ktquez <ktquez@gmail.com>
2020-01-13 08:11:22 -0300

    Update SvgIcon

--- src/components/SvgIcon.vue
+++ src/components/SvgIcon.vue
@@ -1,7 +1,10 @@
 <template>
-  <svg :class="className" xmlns="http://www.w3.org/2000/svg">
+  <svg aria-hidden="true" :class="className" xmlns="http://www.w3.org/2000/svg">
     <title v-if="title">{{ title }}</title>
-    <use :xlink:href="iconPath" xmlns:xlink="http://www.w3.org/1999/xlink"/>
+    <use
+      v-bind="$attrs"
+      :xlink:href="iconPath"
+      xmlns:xlink="http://www.w3.org/1999/xlink"/>
   </svg>
 </template>
 
