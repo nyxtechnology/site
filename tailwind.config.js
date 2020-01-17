427ae480b0ba10ae15b0992162e00924c30ee84f
Alan Ktquez <ktquez@gmail.com>
2020-01-17 13:31:13 -0300

    Add styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
       colors: {
         primary: '#4562C1',
         accent: '#E44F78'
+      },
+      boxShadow: {
+        nyx: '0 6px 4px #EDF2F7',
+        gray100: '0 6px 4px #EDF2F7'
       }
     }
   },
