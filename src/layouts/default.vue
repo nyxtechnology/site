18742dd9845b7bdbfee4c28336d893a4866706dd
Alan Ktquez <ktquez@gmail.com>
2020-01-17 13:32:16 -0300

    Add layouts

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,30 @@
 <template>
-  <router-view :key="$route.fullPath" />
+  <div class="layout flex flex-wrap">
+    <the-left-side class="layout__left-side hidden sm:block" />
+    <div class="layout__content bg-white pt-12 z-10">
+      <div class="container nyx-container">
+        <the-header />
+        <main>
+          <router-view :key="$route.fullPath" />
+        </main>
+      </div>
+    </div>
+    <the-footer class="w-full mt-40" />
+  </div>
 </template>
 
-<style lang="scss" src="@styles/base.scss"></style>
+<script>
+import TheHeader from '@/components/layout/TheHeader'
+import TheLeftSide from '@/components/layout/TheLeftSide'
+
+export default {
+  name: 'DefaultLayout',
+  components: {
+    TheLeftSide,
+    TheHeader,
+    TheFooter: () => import('@/components/layout/TheFooter')
+  }
+}
+</script>
+
+<style lang="scss"></style>
---
+++ src/layouts/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="layout-home flex">
+    <the-left-side class="layout__left-side hidden sm:block" />
+    <div class="layout__content">
+      <the-hero />
+      <router-view :key="$route.fullPath" />
+      <the-footer />
+    </div>
+  </div>
+</template>
+
+<script>
+import TheHero from '@/components/layout/TheHero'
+import TheLeftSide from '@/components/layout/TheLeftSide'
+
+export default {
+  name: 'HomeLayout',
+  components: {
+    TheHero,
+    TheLeftSide,
+    TheFooter: () => import('@/components/layout/TheFooter')
+  }
+}
+</script>
+
+<style lang="scss"></style>
