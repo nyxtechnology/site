38b93f9c7cd221ba044459a2b64adb8b612b64b1
Alan Ktquez <ktquez@gmail.com>
2020-01-17 13:33:03 -0300

    Register global components

---
+++ src/components/_globals.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+
+import AppButton from './AppButton'
+import AppInput from './AppInput'
+import AppTextarea from './AppTextarea'
+import SvgIcon from './SvgIcon'
+
+Vue.component('svg-icon', SvgIcon)
+Vue.component('app-input', AppInput)
+Vue.component('app-button', AppButton)
+Vue.component('app-textarea', AppTextarea)
--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import store from '@/store'
 
 import App from './App.vue'
 import './registerServiceWorker'
+import '@/plugins'
+import '@/components/_globals'
 import i18n from './i18n'
 
 Vue.config.productionTip = false
